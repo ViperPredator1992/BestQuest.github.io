e8dbe6d7a08a54a7507f9b586f6db5292e60c683
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-04-08 19:33:59 +0300

    --- Auto-Git Commit ---

--- src/js/script.js
+++ src/js/script.js
@@ -857,16 +857,20 @@
     };
 })(jQuery, window);
 
-$('.burger').click(function () {
-    if ($(this).next('.collapse:not(.show)').css('display') == 'none') {
-        $(this).next('.collapse:not(.show)').slideDown();
-        $('.burger').addClass('burger-open');
-        $('.navbar-light .navbar-brand').addClass('opacity');
-    } else {
-        $('.burger').removeClass('burger-open');
-        $(this).next('.collapse:not(.show)').slideUp();
-        $('.navbar-light .navbar-brand').removeClass('opacity');
-    }
+$(document).ready(function () {
+   
+    $('.burger').click(function () {
+        if ($(this).next('.collapse:not(.show)').css('display') == 'none') {
+            $(this).next('.collapse:not(.show)').slideDown();
+            $('.burger').addClass('burger-open');
+            $('.navbar-light .navbar-brand').addClass('opacity');
+        } else {
+            $('.burger').removeClass('burger-open');
+            $(this).next('.collapse:not(.show)').slideUp();
+            $('.navbar-light .navbar-brand').removeClass('opacity');
+        }
+
+    });
 
     $("select").selecter();
     
